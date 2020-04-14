64eb506e8ce11cf87e65214002a4e27969fcf043
Abdulraheem Sherif Adavuruku <abdulraheem.sherif@outsourceglobal.com>
2020-04-14 21:22:23 +0100

    done

---
+++ UPDATE_DELETE_IN_DATAGRID_MYSQL/UPDATE_DELETE_IN_DATAGRID_MYSQL/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace UPDATE_DELETE_IN_DATAGRID_MYSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.new_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(581, 278);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(21, 307);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(129, 33);
+            this.save_btn.TabIndex = 1;
+            this.save_btn.Text = "UPDATE";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // new_btn
+            // 
+            this.new_btn.Location = new System.Drawing.Point(156, 308);
+            this.new_btn.Name = "new_btn";
+            this.new_btn.Size = new System.Drawing.Size(138, 31);
+            this.new_btn.TabIndex = 2;
+            this.new_btn.Text = "ADD NEW";
+            this.new_btn.UseVisualStyleBackColor = true;
+            this.new_btn.Click += new System.EventHandler(this.new_btn_Click);
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(300, 309);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(158, 31);
+            this.delete_btn.TabIndex = 3;
+            this.delete_btn.Text = "DELETE";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(481, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "REFRESH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(23, 345);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(371, 102);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Check Box";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 466);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.delete_btn);
+            this.Controls.Add(this.new_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Button new_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ UPDATE_DELETE_IN_DATAGRID_MYSQL/UPDATE_DELETE_IN_DATAGRID_MYSQL/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UPDATE_DELETE_IN_DATAGRID_MYSQL
+{
+    public partial class Form1 : Form
+    {
+        /*declare all the connection and mysqladapter , mysql command 
+        mysqldatapter here...so that they will remain public ToolBar the whole
+        form so that at any time you can reload the datadapter from what you have in the        database*/
+        MySqlConnection con = new MySql.Data.MySqlClient.MySqlConnection();
+        MySqlCommand sCommand;
+        MySqlDataAdapter sAdapter;
+        MySqlCommandBuilder sBuilder;
+        DataSet sDs;
+        DataTable sTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new MySql.Data.MySqlClient.MySqlConnection(@"server=localhost;username=root;password=;database=filemanagement;port=3306");
+            reload_form();
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reload_form();
+        }
+        public void reload_form()
+        {
+            string sql = "SELECT * FROM csharptest";
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            sCommand = new MySqlCommand(sql, con);
+            sAdapter = new MySqlDataAdapter(sCommand);
+            sBuilder = new MySqlCommandBuilder(sAdapter);
+            sDs = new DataSet();
+            sAdapter.Fill(sDs, "csharptest");
+            sTable = sDs.Tables["csharptest"];
+            con.Close();
+            dataGridView1.DataSource = sDs.Tables["csharptest"];
+            dataGridView1.ReadOnly = true;
+
+            save_btn.Enabled = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void delete_btn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to delete this row ?", "Delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+                sAdapter.Update(sTable);
+            }
+        }
+
+        private void new_btn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = false;
+            save_btn.Enabled = true;
+            new_btn.Enabled = false;
+            delete_btn.Enabled = false;
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            sAdapter.Update(sTable);
+            dataGridView1.ReadOnly = true;
+            save_btn.Enabled = false;
+            new_btn.Enabled = true;
+            delete_btn.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.ColumnCount = 3;
+
+            dataGridView2.Columns[0].Name = "Product ID";
+
+            dataGridView2.Columns[1].Name = "Product Name";
+
+            dataGridView2.Columns[2].Name = "Product Price";
+
+
+
+            string[] row = new string[] { "1", "Product 1", "1000" };
+
+            dataGridView2.Rows.Add(row);
+
+            row = new string[] { "2", "Product 2", "2000" };
+
+            dataGridView2.Rows.Add(row);
+
+            row = new string[] { "3", "Product 3", "3000" };
+
+            dataGridView2.Rows.Add(row);
+
+            row = new string[] { "4", "Product 4", "4000" };
+
+            dataGridView2.Rows.Add(row);
+
+
+
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+
+            dataGridView2.Columns.Add(chk);
+
+            chk.HeaderText = "Check Data";
+
+            chk.Name = "chk";
+
+            dataGridView2.Rows[2].Cells[3].Value = true;
+        }
+    }
+}
